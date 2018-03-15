afd0480a8b5153d6c50cb9fd87126a816a85a078
yexuejc <1107047387@qq.com>
2018-03-15 15:35:05 +0800

    add aop demo

--- util-api-demo/src/main/java/com/yexuejc/util/api/demo/UtilApiDemoApplication.java
+++ util-api-demo/src/main/java/com/yexuejc/util/api/demo/UtilApiDemoApplication.java
@@ -1,12 +1,12 @@
 package com.yexuejc.util.api.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class UtilApiDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UtilApiDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UtilApiDemoApplication.class, args);
+    }
 }
